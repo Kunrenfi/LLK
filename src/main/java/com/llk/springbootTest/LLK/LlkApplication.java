2df0af1d13c3fcb2b30f3003c591eb1a232b146a
Teanoo <532534854@qq.com>
2018-12-01 14:36:41 +0800

    ilalalalala

--- src/main/java/com/llk/springbootTest/LLK/LlkApplication.java
+++ src/main/java/com/llk/springbootTest/LLK/LlkApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LlkApplication {
+	public String hhhh;
+	public String lalala;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LlkApplication.class, args);
