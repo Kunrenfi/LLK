06fcf8df5f5a2ac7209139ed87208726516f331e
Kunrenfi <a1243729005@qq.com>
2018-12-01 13:34:15 +0800

    Initial commit

---
+++ src/main/java/com/llk/springbootTest/LLK/LlkApplication.java
@@ -0,0 +1,12 @@
+package com.llk.springbootTest.LLK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/llk/springbootTest/LLK/LlkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.llk.springbootTest.LLK;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LlkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
